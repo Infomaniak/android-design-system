59f3f0d40b0f12862f4560dbab54258c56141a2f
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-06-09 16:20:21 +0200

    chore: Remove useless composite build values in build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-group = "com.infomaniak.designsystem.buildlogic"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
-
 dependencies {
     compileOnly(libs.android.gradle.plugin)
 }
